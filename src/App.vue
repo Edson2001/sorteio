cebede40feed9223f5149f184bda2d6e9128984b
Edson  Dos Santos <djiedson413@gmail.com>
2021-02-19 16:13:02 +0100

    Atualização do carrousel

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,51 @@
 <template>
   <div id="app">
 
+    <menu-vue></menu-vue>
+
     <div class="top">
 
     </div>
-    
-    <div class="carousel">
-      <div class="carouselbox" >
+
+    <div class="limite">
+      <div class="carousel ">
+
+        <div class="title">
+          <span>
+           #DC FILMES
+          </span>
+        </div>
+
+        <div class="carouselbox c1" >
+          
+        </div>
+
         
+
+        <a class="switchLeft sliderButton" @click="sliderScrollLeft()"><i class="icofont-rounded-left"></i></a>
+        <a class="switchRight sliderButton" @click="sliderScrollRight()"><i class="icofont-rounded-right"></i></a>
       </div>
 
-      <a class="switchLeft sliderButton" @click="sliderScrollLeft()">-</a>
-      <a class="switchRight sliderButton" @click="sliderScrollRight()">-</a>
+      <div class="carousel ">
+
+        <div class="title">
+          <span>
+            #MARVEL FILMES
+          </span>
+        </div>
+
+        <div class="carouselbox c2" >
+          
+        </div>
+
+        
+
+        <a class="switchLeft sliderButton" @click="sliderScrollLeft2()"><i class="icofont-rounded-left"></i></a>
+        <a class="switchRight sliderButton" @click="sliderScrollRight2()"><i class="icofont-rounded-right"></i></a>
+      </div>
     </div>
+    
+    
 
     
     
@@ -22,10 +55,13 @@
 
 <script>
 import axios from 'axios'
+import './assets/css/carousel.css'
+import menuVue from './components/menu.vue'
 
 
 export default {
   name: 'App',
+  components: {menuVue},
   data(){
     return{
       url: "https://api.themoviedb.org/4/list/3?page=1&api_key=5a1939e7ee4fd5d2be953c58f1787222",
@@ -34,6 +70,7 @@ export default {
        scrollPerClick: 0,
        ImagePadding: 20,
        scrollAmount: 0,
+       title: ''
     }
   },
 
@@ -47,14 +84,15 @@ export default {
     })
 
     this.sliders = document.querySelector(".carouselbox")
-
+    
      this.showMoviesData();
   },
 
   methods:{
     
     sliderScrollLeft() {
-      document.querySelector(".carouselbox").scrollTo({
+      
+      document.querySelector(".c1").scrollTo({
         top: 0,
         left: (this.scrollAmount -= this.scrollPerClick),
         behavior: "smooth",
@@ -63,37 +101,89 @@ export default {
       if (this.scrollAmount < 0) {
         this.scrollAmount = 0;
       }
+    },
 
-      console.log("Scroll Amount  l: ", this.scrollAmount);
+    sliderScrollLeft2() {
+      document.querySelector(".c2").scrollTo({
+        top: 0,
+        left: (this.scrollAmount -= this.scrollPerClick),
+        behavior: "smooth",
+      });
+      if (this.scrollAmount < 0) {
+        this.scrollAmount = 0;
+      }
     },
 
   sliderScrollRight() {
  
-    if (this.scrollAmount <= document.querySelector(".carouselbox").scrollWidth - document.querySelector(".carouselbox").clientWidth) {
-      document.querySelector(".carouselbox").scrollTo({
+    if (this.scrollAmount <= document.querySelector(".c1").scrollWidth - document.querySelector(".c2").clientWidth) {
+      document.querySelector(".c1").scrollTo({
         top: 0,
         left: (this.scrollAmount += this.scrollPerClick),
         behavior: "smooth",
       });
     }
-    console.log("Scroll Amount r: ", this.scrollAmount);
+
+
+   
   },
 
-// For showing dynamic contents only
+  sliderScrollRight2() {
+
+
+    if (this.scrollAmount <= document.querySelector(".c2").scrollWidth - document.querySelector(".c2").clientWidth) {
+      document.querySelector(".c2").scrollTo({
+        top: 0,
+        left: (this.scrollAmount += this.scrollPerClick),
+        behavior: "smooth",
+      });
+    }
+    
+  },
+
+
     async  showMoviesData() {
-      //const api_key = "47071116ad3913e09389a88eb1f895b6";
+   
       var result = await axios.get(
         "https://api.themoviedb.org/4/list/3?page=1&api_key=5a1939e7ee4fd5d2be953c58f1787222"
       );
 
+      var result2 = await axios.get(
+        "https://api.themoviedb.org/4/list/1?page=1&api_key=5a1939e7ee4fd5d2be953c58f1787222"
+      );
+
+
       result = result.data.results;
+      result2 = result2.data.results;
 
       result.map( (cur, index) => {
       
-        document.querySelector(".carouselbox").insertAdjacentHTML(
+        document.querySelector(".c1").insertAdjacentHTML(
           "beforeend",
-          `<img class="img-${index} slider-img" src="http://image.tmdb.org/t/p/w185/${cur.poster_path}" /> `
+          ` <div class="">
+              
+              <img class="img-${index} slider-img" src="http://image.tmdb.org/t/p/w185/${cur.poster_path}" />
+            
+              
+            </div>
+          `
         );
+
+      });
+
+      result2.map( (cur, index) => {
+
+        document.querySelector(".c2").insertAdjacentHTML(
+          "beforeend",
+          ` <div class="">
+              
+              <img class="img-${index} slider-img" src="http://image.tmdb.org/t/p/w185/${cur.poster_path}" />
+            
+              
+            </div>
+          `
+        );
+
       });
 
       this.scrollPerClick = document.querySelector(".img-1").clientWidth + 20;
@@ -109,107 +199,14 @@ export default {
   padding: 0;
   margin: 0;
 }
-/*
-#app {
-  background: #222;
-  padding: 0;
-  margin: 0;
-}*/
-* {
-  margin: 0;
-  padding: 0;
-}
+
 
 body {
-  height: 100vh;
-  width: 100%;
-  background-color: #262626;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-wrap: wrap;
-}
-/*
-.top{
-  width: 100%;
-  box-shadow: 0 6px 6px rgba(0,0,0,.4);
-  height: 500px;
-  background: url('https://i1.wp.com/thefutureoftheforce.com/wp-content/uploads/2020/08/Zack-Snyders-Justice-League.jpg?fit=3200%2C1800&ssl=1');
-  background-size: cover;
  
-}*/
+  background-color: #262626;
+  font-family: 'Roboto';
 
-
-.netflix-logo {
-  height: 15vw;
-  width: 100%;
-  text-align: center;
 }
 
-.netflix-logo img {
-  height: 100%;
-  /* width: 100%; */
-}
 
-.carousel {
-  height: 250px;
-  width: 80%;
-  position: relative;
-}
-.carousel .switchLeft,
-.carousel .switchRight {
-  color: white;
-  font-weight: bold;
-  height: 100%;
-  width: 45px;
-  line-height: 250px;
-  font-size: 25px;
-  text-align: center;
-  background-color: lightgreen;
-  font-family: sans-serif;
-  top: 0;
-  z-index: 3;
-}
-
-.carousel .switchLeft {
-  position: absolute;
-  left: -50px;
-}
-
-.carousel .switchRight {
-  position: absolute;
-
-  right: -50px;
-}
-
-.carouselbox {
-  height: 250px;
-  width: auto;
-  overflow: hidden;
-  white-space: nowrap;
-  display: inline-block;
-  text-align: center;
-  padding-bottom: 10px !important;
-  display: flex;
-  align-items: center;
-}
-
-.carouselbox img {
-  min-width: 147px;
-  max-width: 147px;
-  height: 200px;
-  background-size: cover;
-  background-color: lightgreen;
-  margin: 5px 10px;
-  cursor: pointer;
-  transition: 0.5s ease;
-  z-index: 2;
-
-  /* margin: 15px; */
-}
-
-.carouselbox img:hover {
-  transform: scale(1.4);
-  z-index: 5;
-}
 </style>
---
+++ src/components/menu.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <div class="menu">
+            <a href="" class="logo">
+                <h1>CollFlix</h1>
+            </a>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="">Séries</a>
+                    </li>
+                    <li>
+                        <a href="">Filmes</a>
+                    </li>
+                    <li>
+                        <a href="">Minha Lista</a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src="https://avatars.githubusercontent.com/u/39365797?s=460&u=4ebebc0bce1839222a2ce180b0b4aa0e13d45b1a&v=4" alt="" srcset="">
+                            <span>Dos Santos</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</template>
+
+<script>
+import '../assets/css/menu.css'
+export default {
+    name: 'menuVue'
+}
+</script>
