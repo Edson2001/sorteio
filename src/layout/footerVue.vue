c6127e5abba977493109bde1d1731a3000130251
edson2001 <djiedson413@gmai.com>
2022-11-27 18:17:06 +0100

    fix: cards carousel
    cards carousel

---
+++ src/layout/footerVue.vue
@@ -0,0 +1,11 @@
+<template>
+    <footer>
+      <b> <a href="https://github.com/Edson2001/collflix">CollFlix</a> - 2021 | DADOS DE FILMES:  <a href="https://www.themoviedb.org/">TMDB</a> </b>
+      <p>Criado por <a href="https://github.com/Edson2001">Edson Dos Santos Bandola</a> </p>
+    </footer>
+</template>
+<script>
+export default {
+    name: "footerVue"
+}
+</script>
--- src/views/Query.vue
+++ src/views/Query.vue
@@ -23,6 +23,7 @@
                 </div>
             </div>
         </div>
+        <footerVueVue />
     </div>
 </template>
 
